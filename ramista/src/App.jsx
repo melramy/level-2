f372bbac87f3aa517b2ae889814c73cbca564b59
elramygithub <mrelramy@gmail.com>
2024-01-24 21:40:30 +0200

    level 2

---
+++ ramista/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <button>count is 0</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
