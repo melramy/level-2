7308d2063bb4704e041933290321b6501a9e415a
elramygithub <mrelramy@gmail.com>
2024-01-25 09:55:52 +0200

    dark-light

--- ramista/src/App.jsx
+++ ramista/src/App.jsx
@@ -1,21 +1,55 @@
 import "./App.css";
+import "./theme.css";
+
 import { useState } from "react";
 
 function App() {
   const [person, setPerson] = useState("Mohamed Elramy");
   const [age, setage] = useState("28");
-const [count, setcount] = useState(0);
-
+  const [count, setcount] = useState(0);
+  const [theme, settheme] = useState("");
 
   const changeAge = () => {
     setage("33");
-
-
-  }
+  };
   return (
     <>
-      <div className="App">
-        <h2>My Name is {person}</h2>
+      <div className={`App ${theme}`}>
+        <div>
+          <button
+            onClick={() => {
+              settheme("");
+            }}
+            style={{ marginRight: "26px" }}
+          >
+            Light
+          </button>
+          <button
+            onClick={() => {
+              settheme("dark");
+            }}
+            style={{ marginRight: "26px" }}
+          >
+            Dark
+          </button>
+          <button
+            onClick={() => {
+              settheme("grey");
+            }}
+            style={{ marginRight: "26px" }}
+          >
+            Grey
+          </button>
+          <button
+            onClick={() => {
+              settheme("pink");
+            }}
+          >
+            Pink
+          </button>
+        </div>
+
+        <h2  style={{ marginTop: "66px" }} >My Name is {person}</h2>
         <button
           onClick={() => {
             setPerson("elramy we bas ");
@@ -33,11 +67,13 @@ const [count, setcount] = useState(0);
         <br />
         <br />
         <br />
-        <button onClick={() => {
-          setcount(count+1);
-        }}>Count is {count}</button>
-
-
+        <button
+          onClick={() => {
+            setcount(count + 1);
+          }}
+        >
+          Count is {count}
+        </button>
       </div>
     </>
   );
